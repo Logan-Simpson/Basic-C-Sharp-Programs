0b7d1ca1f6603a836a7d360f0c923bc9628b7c55
Logan-Simpson <loganehsimpeh@gmail.com>
2021-01-19 10:30:02 -0800

    Update AnonIncome

--- AnonIncome/AnonIncome/Program.cs
+++ AnonIncome/AnonIncome/Program.cs
@@ -20,10 +20,10 @@ namespace AnonIncome
             int rate2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
             int hours2 = Convert.ToInt32(Console.ReadLine());
-            int salary1 = rate1 * hours1;
+            int salary1 = rate1 * hours1 * 52;
             Console.WriteLine("Person 1 annual salary: " + salary1);
             Console.ReadLine();
-            int salary2 = rate2 * hours2;
+            int salary2 = rate2 * hours2 * 52;
             Console.WriteLine("Person 2 annual salary: " + salary2);
             Console.ReadLine();
             Console.WriteLine("Does person 1 make more money than person 2?");
