11c531596cc6e8e475f5b6c2e330e7ecacf96fe4
Logan-Simpson <loganehsimpeh@gmail.com>
2021-01-22 12:11:10 -0800

    Fixed Class223

--- Class223/Class223/Math.cs
+++ Class223/Class223/Math.cs
@@ -8,10 +8,21 @@ namespace Class223
 {
     public class Math
     {
-        public int Add { get; set; }
-        public int Subtract { get; set; }
-        public int Multiply { get; set; }
-
-        
-    }
+        public int Add(int input)
+        {
+            int result = input + 20;
+            return result;
+        }
+        public int Subtract(int input)
+        {
+            int result = input - 50;
+            return result;
+        }
+        public int Multiply(int input)
+        {
+            int result = input * 37;
+            return result;
+        }
+     
+     }
 }
--- Class223/Class223/Program.cs
+++ Class223/Class223/Program.cs
@@ -8,17 +8,22 @@ namespace Class223
 {
     class Program
     {
-        public static int x;
-        public static int y;
-        public static int z;
-
+     
         static void Main(string[] args)
         {
-            Math math = new Math();
-            math.Add = x;
-            math.Subtract = y;
-            math.Multiply = z;
+            Math math1 = new Math();
+            Console.WriteLine("Please enter a number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            math1.Add(x);
+            math1.Subtract(x);
+            math1.Multiply(x);
+
+            Console.WriteLine("Your number + 20 is " + math1.Add(x));
+            Console.WriteLine("Your number minus 50 is " + math1.Subtract(x));
+            Console.WriteLine("Your number times 37 is " + math1.Multiply(x));
+            Console.ReadLine();
+
+            
         }
-        
     }
 }
