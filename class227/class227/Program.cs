9a2a1728281843df9f40477b589950d11329f64d
Logan-Simpson <loganehsimpeh@gmail.com>
2021-01-26 09:57:24 -0800

    commit class227

---
+++ class227/class227/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            arithmetic math1 = new arithmetic();
+            Console.WriteLine("Please enter a number.");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you want, enter a second number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            math1.AddTwo(int i, object input2);
+            return 
+        }
+    }
+}
---
+++ class227/class227/arithmetic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class227
+{
+    class arithmetic
+    {
+        public class mathClass
+        {
+            public int AddTwo(int input, int input2 = 20)
+            {
+                int result = input + input2;
+                return result;
+            }
+
+
+        }
+    }
+}
