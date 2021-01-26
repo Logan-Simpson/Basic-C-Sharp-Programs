7e0286b1f272574c2c2ecc3660f58c55bd0b80aa
Logan-Simpson <loganehsimpeh@gmail.com>
2021-01-25 17:37:30 -0800

    Commit newClass226

---
+++ newClass226/newClass226/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newClass226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass math2 = new mathClass();
+            Console.WriteLine("Please enter a number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            mathClass.Add(x);
+            mathClass.Add(x);
+            mathClass.Add(x);
+
+            Console.WriteLine("Your number + 20 is " + mathClass.Add(x));
+            Console.WriteLine("Your number minus 50 is " + mathClass.Add(x));
+            Console.WriteLine("Your number times 37 is " + mathClass.Add(x));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ newClass226/newClass226/mathClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newClass226
+{
+    public class mathClass
+        {
+            public int Add(int input)
+            {
+                int result = input + 20;
+                return result;
+            }
+            public decimal Add(decimal input)
+            {
+                decimal result = input + 50;
+                return result;
+            }
+            public string Add(string input)
+            {
+                string result = input + 37;
+                return result;
+            }
+        }
+    }
+
+
